762e16bd126a15afe859757de2ee971cc40919e0
Arif-0087 <mdarifbillah0087@gmail.com>
2025-07-30 21:57:02 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-center text-gray-800">
+        GitHub AI Analyzer Frontend
+      </h1>
+    </div>
+  );
+}
